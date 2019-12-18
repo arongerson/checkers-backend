5e379a5042850a6c9d03f629eaf0a0e28deb8426
arongerson <gerson.aron@yahoo.com>
2019-12-18 08:37:31 -0600

    Set up websockets

---
+++ src/com/aronek/checkers/Message.java
@@ -0,0 +1,30 @@
+package com.aronek.checkers;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public final class Message {
+
+    @JsonProperty("username")
+    private final String userName;
+    @JsonProperty
+    private final String message;
+
+    @JsonCreator
+    public Message(@JsonProperty("username") final String userName, @JsonProperty("message") final String message) {
+        Objects.requireNonNull(userName);
+        Objects.requireNonNull(message);
+        this.userName = userName;
+        this.message = message;
+    }
+
+    String getUserName() {
+        return this.userName;
+    }
+
+    String getMessage() {
+        return this.message;
+    }
+}
