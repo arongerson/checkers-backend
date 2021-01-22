dbb7c030de4ef02d4fed8dda0e7152513fa2041f
arongerson <gerson.aron@yahoo.com>
2021-01-22 09:04:24 -0500

    created a rules class

---
+++ src/com/aronek/checkers/entity/Rules.java
@@ -0,0 +1,10 @@
+package com.aronek.checkers.entity;
+
+public class Rules {
+	public boolean canPieceCaptureBackwards = true;
+	public boolean canKingMoveMoreThanOneStep = true;
+	public boolean shouldPieceContinueCapturingAfterFarthestRow = true;
+	public boolean shouldCaptureWhenPossible = true;
+	public boolean shouldCaptureMaxPossible = true;
+	public boolean shouldDiscardCapturedPieceMomentarily = true;
+}
